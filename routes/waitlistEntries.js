e832228ea46ee2bb9d440e443b429d31489774bf
Rafael Alba <rafael@tcell.io>
2016-05-17 15:28:03 -0700

    add an endpoint to seed the mongodb

--- routes/waitlistEntries.js
+++ routes/waitlistEntries.js
@@ -5,7 +5,6 @@ const WaitlistEntry = require('../models/waitlistEntry');
 
 router.get('/', function(req, res, next) {
   WaitlistEntry.find({}, function(err, waitlistEntries){
-    console.log(waitlistEntries);
     res.render('waitlist_entries/index', {
       title: 'Waitlist Entries',
       waitlistEntries: waitlistEntries
@@ -16,6 +15,22 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
   new WaitlistEntry({email: req.body.email}).save(function (err) {
     res.send(err);
+  });
+});
+
+router.post('/seed', function(req, res, next) {
+  WaitlistEntry.remove({}, function(err) {
+    if (err) {
+      res.send(err)
+    } else {
+      var seeds = []
+      for(count = 0; count < 1000; count++){
+        seeds.push({email: ("waitlist+" + count + "@tcell.io")})
+      }
+      WaitlistEntry.collection.insert(seeds, function(err, docs) {
+        res.send(docs)
+      });
+    }
   });
 });
 
