f0213c9eda2af420eb5761dc2b62523938889de5
Rafael Alba <rafael@tcell.io>
2016-05-17 15:34:58 -0700

    add a status page for pinging purposes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/ping', function(req, res, next) {
+  res.send({status: "alive"})
+});
+
 module.exports = router;
