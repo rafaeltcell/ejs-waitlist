e8d1cc16e575b8091d885ec384eea491b601f5b7
Rafael Alba <rafael@tcell.io>
2016-05-18 11:50:02 -0700

    can now register, login, and logout

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var Account = require('../models/account');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Waitlist', user: req.user });
 });
 
 router.get('/ping', function(req, res, next) {
   res.send({status: "alive"})
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register', {});
+});
+
+router.post('/register', function(req, res) {
+  Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+    if (err) {
+      return res.render('register', { account : account });
+    }
+
+    passport.authenticate('local')(req, res, function () {
+      res.redirect('/');
+    });
+  });
+});
+
+router.get('/login', function(req, res) {
+  res.render('login', { user : req.user });
+});
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+  res.redirect('/');
+});
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
